f18580b293a8510427b1ec4ff9fe66fe8eb2488d
BreisOne <b.mascat@gmail.com>
2024-04-08 18:31:02 +0200

    feat(symf_project): 8.1 task solution

--- DWCS/UD8/symf_project/assets/app.js
+++ DWCS/UD8/symf_project/assets/app.js
@@ -6,5 +6,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ DWCS/UD8/symf_project/src/Controller/TableController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TableController extends AbstractController
+{
+    #[Route('/table', name: 'app_table')]
+    public function index(): Response
+    {
+        return $this->render('table/index.html.twig', [
+            'controller_name' => 'TableController',
+        ]);
+    }
+
+    #[Route('/table/{fils<([1-9]+\d*)>?4}/{cols<([1-9]\d*)>?4}', name: 'app_table')]
+    public function createTable(int $fils, int $cols): Response
+    {
+        $table = [];
+        for ($i = 0; $i < $fils; $i++) {
+            $row = array();
+            for ($j = 0; $j < $cols; $j++) {
+                $num = random_int(0, 100);
+                $row[] = $num;
+            }
+            $table[] = $row;
+        }
+
+        return $this->render('table/index.html.twig', [
+            'controller_name' => 'TableController',
+            'table_array' => $table,
+            'fils' => $fils,
+            'cols'=> $cols,
+        ]);
+    }
+}
+
